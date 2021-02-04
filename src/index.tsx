75c710f27b76a35f8fa342a1c1fc8288b9f8587a
faiq3yvazli <faiq3yvazli@gmail.com>
2021-02-04 21:04:35 +0400

    Clean up the project

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import reportWebVitals from './reportWebVitals'
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+import * as WebVitals from './reportWebVitals'
 
 const app = <h1>Test</h1>
 const node = document.getElementById('root')
 
 ReactDOM.render(app, node)
 
-reportWebVitals()
+WebVitals.report()
