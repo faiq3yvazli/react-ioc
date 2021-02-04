aa569694aca9b36b1fb72f38dcf198d154d64035
faiq3yvazli <faiq3yvazli@gmail.com>
2021-02-04 21:32:47 +0400

    Implement import alias

--- config/config.overrides.js
+++ config/config.overrides.js
@@ -1,7 +1,13 @@
-const { override, addBabelPlugin } = require('customize-cra')
+const path = require('path')
+const { override, addBabelPlugin, addWebpackAlias } = require('customize-cra')
 
 module.exports = override(
   addBabelPlugin(['babel-plugin-transform-typescript-metadata']),
   addBabelPlugin(['@babel/plugin-proposal-decorators', { legacy: true }]),
   addBabelPlugin(['@babel/plugin-proposal-class-properties', { loose: true }]),
+  addWebpackAlias({
+    '@modules': path.resolve(__dirname, '..', 'src', 'modules'),
+    '@config': path.resolve(__dirname, '..', 'src', 'config'),
+    '@core': path.resolve(__dirname, '..', 'src', 'core'),
+  }),
 )
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
-import * as WebVitals from './config/report-web-vitals'
+import * as WebVitals from '@config/report-web-vitals'
 
 const app = <h1>Test</h1>
 const node = document.getElementById('root')
